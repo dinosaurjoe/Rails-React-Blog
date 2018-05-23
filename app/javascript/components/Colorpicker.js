69fe11e813c121763ca11d377aa387b8e7124337
Joe Schafer <joeschaferastula@gmail.com>
2018-05-23 10:41:21 -0500

    update color form based on colorpicker state

--- app/javascript/components/Colorpicker.js
+++ app/javascript/components/Colorpicker.js
@@ -7,11 +7,16 @@ class Colorpicker extends React.Component {
   constructor(props) {
     super(props);
 
+    this.state = { selector: props.selector };
     this.handleChange = this.handleChange.bind(this);
   }
 
   handleChange = (color) => {
     document.body.style.background = color.hex;
+
+    let elem = document.querySelector('#' + this.state.selector);
+
+    elem.value = color.hex;
   }
 
   render () {
